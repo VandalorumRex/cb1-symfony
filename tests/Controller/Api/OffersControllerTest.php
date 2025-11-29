d64b5b40d99200629655e4b29d0937c6476ae8ed
Mansur <gho-mansur@yandex.ru>
2025-11-29 23:21:39 +0300

    test: PUT /api/offers

--- tests/Controller/Api/OffersControllerTest.php
+++ tests/Controller/Api/OffersControllerTest.php
@@ -18,7 +18,7 @@ class OffersControllerTest extends WebTestCase
     public function testStore(): void
     {
         $client = static::createClient();
-        $client->xmlHttpRequest('POST', '/api/offers', ['name' => 'Fabien']);;
+        $client->xmlHttpRequest('POST', '/api/offers', ['name' => 'Fabien']);
         $this->assertResponseStatusCodeSame(Response::HTTP_BAD_REQUEST);
         
         $content='{
@@ -40,7 +40,36 @@ class OffersControllerTest extends WebTestCase
               "apartment": ""
             }
           }';
-        $client->request('POST', '/api/offers', content: $content);;
+        $client->request('POST', '/api/offers', content: $content);
         $this->assertResponseStatusCodeSame(Response::HTTP_CREATED);
+    }
+    
+    public function testUpdate(): void
+    {
+        $client = static::createClient();
+        $client->xmlHttpRequest('PUT', '/api/offers/1', ['name' => 'Fabien']);
+        $this->assertResponseStatusCodeSame(Response::HTTP_BAD_REQUEST);
+        
+        $content='{
+            "type": "продажа",
+            "propertyType": "жилая",
+            "category": "дача",
+            "garageType": "гараж",
+            "lotNumber": "1",
+            "cadastralNumber": "16:00:111222:789",
+            "url": "https://example.com",
+            "creationDate": "",
+            "location": {
+              "country": "Россия",
+              "region": "Республика Татарстан",
+              "district": "Зеленодольский",
+              "localityName": "Осиново",
+              "subLocalityName": "Радужный",
+              "address": "Садовая 9",
+              "apartment": ""
+            }
+          }';
+        $client->request('PUT', '/api/offers/2', content: $content);
+        $this->assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
     }
 }
